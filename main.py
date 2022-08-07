2c4d79c8afafae1789f9966f2de150e339c31d42
YvetteFB <yvettefionabaraza@gmail.com>
2022-08-07 20:16:51 +0300

    init

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+# Addition
 def add(x, y):
     return x + y
 
 
+# Subtraction
 def subtract(x, y):
     return x - y
 
 
+# Multiplication
 def multiply(x, y):
     return x * y
 
 
+# Division
 def divide(x, y):
     return x / y
 
